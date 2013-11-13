6678d1010434d5c4631ec0844752d43fa641eb8c
dixe <niko.fris@gmail.com>
2013-11-13 14:29:46 +0100

    ConfgiChanger

--- ConfigChanger.hs
+++ ConfigChanger.hs
@@ -1,5 +1,4 @@
-module ConfigChanger(changeSeason, changeEpisode,add1Season,
-add1Episode) where
+module ConfigChanger(changeSeason, changeEpisode,add1Episodes) where
 
 import ConfigData
 
@@ -18,6 +17,16 @@ add1Season (DownData title s e) = DownData title (s+1) e
 add1Season Empty = Empty
 
 -- increment episode with 1
-add1Episode :: Download -> Download
-add1Episode (DownData title s e) = DownData title s (e+1)
-add1Episode Empty = Empty
+add1E :: Download -> Download
+add1E (DownData title s e) = DownData title s (e+1)
+add1E Empty = Empty
+
+-- add 1 to episodes, where magnet is not ""
+add1Episodes :: [Download] -> [Bool] -> [Download]
+add1Episodes d m = reverse (add1Episodes' d m)
+
+add1Episodes' :: [Download] -> [Bool] -> [Download]
+add1Episodes' (d:ds) (False:ms) = (d):(add1Episodes' ds ms)
+add1Episodes' (d:ds) (True:ms) = (add1E d):(add1Episodes' ds ms)
+add1Episodes' [] _ = []
+add1Episodes' ds [] = ds
